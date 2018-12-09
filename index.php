ef07b5bdb1a0e02381d1cb5f6108ed8d6672494f
Matt Hansen <hans1312@gmail.com>
2018-12-09 15:11:26 -0600

    fifteen minutes into second video

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Shadows+Into+Light+Two" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+
+    <title>To Do</title>
+</head>
+<body>
+    <div class="list">
+        <h1 class="header">To Do</h1>
+        <ul class="items">
+            <li><span class="item">Buy Groceries</span></li>
+            <li><span class="item done">Learn PHP</span></li>
+        </ul>
+        <form class="item-add" action="add.php" method="post">
+            <input type="text" name="name" placeholder="Type a new task here" class="input" autocomplete="off" required>
+            <input type="submit" value="Add" class="submit">
+        </form>
+
+    </div>
+</body>
+</html>
